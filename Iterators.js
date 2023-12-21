369c2f279a852235d9cb59b65d8b91f4d81f98db
Mark Ward <108774951+WardMark@users.noreply.github.com>
2023-12-21 14:21:00 -0600

    Create Iterators.js
    Common array iterators

---
+++ Iterators.js
@@ -0,0 +1,22 @@
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined and goes through each item
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array based on a boolean check
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array after doing something to it
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a single boolean value
+nums.every(num => num < 0);
